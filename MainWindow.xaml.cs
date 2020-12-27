a94e6a42ccd9c4660f4e0d6020e0be4c957f45c6
gultekin kurtar <gultekinkurtar@gmail.com>
2020-12-27 19:11:38 +0300

    sdf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,23 +16,25 @@ namespace K4ACalibration
         {
             InitializeComponent();
             Console.WriteLine("device tests");
-            Color_Image.Width = 200;
-            Color_Image.Height = 200;
-
+            //Color_Image.Width = 200;
+            //Color_Image.Height = 200;
             Task.Run(() => Go());
         }
 
-        Int32Rect color_rect;
-        int color_stride;
-        WriteableBitmap color_bitmap;
+        //Int32Rect color_rect;
+        //int color_stride;
+        //WriteableBitmap color_bitmap;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private async Task Go()
         {
             try
             {
                 using (Device device = Device.Open())
                 {
-
                     //MessageBox.Show("device open start for " + device.SerialNum);
                     Console.WriteLine("device open start for " + device.SerialNum);
 
@@ -54,45 +56,31 @@ namespace K4ACalibration
                         i++;
                         capture1 = device.GetCapture();
 
-                        int color_width = device.GetCalibration().ColorCameraCalibration.ResolutionWidth;
-                        int color_height = device.GetCalibration().ColorCameraCalibration.ResolutionHeight;
+                        //int color_width = device.GetCalibration().ColorCameraCalibration.ResolutionWidth;
+                        //int color_height = device.GetCalibration().ColorCameraCalibration.ResolutionHeight;
 
                         //MessageBox.Show("device capture null check #" + i + " is " + (null == capture1)
                         //    + "width " + color_width + ", and height " + color_height);
-
-
                         // MessageBox.Show("width " + color_width + ", and height " + color_height);
 
-                        const int color_channles = 4;
-                        color_stride = color_width * sizeof(byte) * color_channles;
-                        color_rect = new Int32Rect(0, 0, color_width, color_height);
-                        color_bitmap = new WriteableBitmap(color_width, color_height, 96.0, 96.0, PixelFormats.Bgra32, null);
+                        //const int color_channles = 4;
+                        //color_stride = color_width * sizeof(byte) * color_channles;
+                        //color_rect = new Int32Rect(0, 0, color_width, color_height);
+                        //color_bitmap = new WriteableBitmap(color_width, color_height, 96.0, 96.0, PixelFormats.Bgra32, null);
 
-
-                        Microsoft.Azure.Kinect.Sensor.Image colorimg = capture1.Color;
-                        //MessageBox.Show("color img " + colorimg.Size.ToString());
-                        byte[] color_buffer = colorimg.Memory.ToArray();
-                        color_bitmap.WritePixels(color_rect, color_buffer, color_stride, 0, 0);
-
+                        //Microsoft.Azure.Kinect.Sensor.Image colorimg = capture1.Color;
+                        //byte[] color_buffer = colorimg.Memory.ToArray();
+                        //color_bitmap.WritePixels(color_rect, color_buffer, color_stride, 0, 0);
 
                         // Create a BitmapSource for the unmodified color image.
                         // Creating the BitmapSource is slow, so do it asynchronously on another thread
-                        Task<BitmapSource> createInputColorBitmapTask = Task.Run(() =>
-                        {
+                        Task<BitmapSource> createInputColorBitmapTask = Task.Run(() => {
                             BitmapSource source = capture1.Color.CreateBitmapSource();
-
                             // Allow the bitmap to move threads
                             source.Freeze();
                             return source;
                         });
 
-
-                        /*
-                        // Bined Image Control
-                        Color_Image.Source = color_bitmap;
-                         Color_Image.Width = color_width;
-                        Color_Image.Height = color_height;
-                        */
 
                         // Wait for both bitmaps to be ready and assign them.
                         BitmapSource inputColorBitmap = await createInputColorBitmapTask.ConfigureAwait(true);
@@ -105,8 +93,6 @@ namespace K4ACalibration
                             this.Color_Image.Source = inputColorBitmap;
                         }));
 
-
-
                         /*Color_Image.Dispatcher.Invoke(() =>
                         {
                             // UI operation goes inside of Invoke
@@ -116,55 +102,6 @@ namespace K4ACalibration
                         });
                         */
                     }
-
-
-                    /*
-                    var calibration = device.GetCalibration();
-                    Transformation transformation = calibration.CreateTransformation();
-
-                    Tracker tracker = Tracker.Create(calibration, new TrackerConfiguration()
-                    {
-                        ProcessingMode = TrackerProcessingMode.Gpu,
-                        SensorOrientation = SensorOrientation.Default
-                    });
-
-                    while (true)
-                    {
-                        Capture capture1 = device.GetCapture();
-
-                        tracker.EnqueueCapture(capture1);
-                        var frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false);
-                        if (frame != null)
-                        {
-                            this.Dispatcher.Invoke(() =>
-                            {
-                                for (uint i = 0; i < frame.NumberOfBodies; i++)
-                                {
-                                    var bodyId = frame.GetBodyId(i);
-                                    var body = frame.GetBody(i);
-
-                                    var handLeft = body.Skeleton.GetJoint(JointId.HandLeft);
-                                    var handRight = body.Skeleton.GetJoint(JointId.HandRight);
-
-                                    var pointLeft = calibration.TransformTo2D(handLeft.Position, CalibrationDeviceType.Depth, CalibrationDeviceType.Color);
-                                    var pointRight = calibration.TransformTo2D(handRight.Position, CalibrationDeviceType.Depth, CalibrationDeviceType.Color);
-
-                                    try
-                                    {
-                                        if (pointLeft.HasValue && pointRight.HasValue)
-                                        {
-                                            Image1.Source = CreateNewBitmap(capture1.Color,
-                                                (int)pointLeft.Value.X, (int)pointLeft.Value.Y,
-                                                (int)pointRight.Value.X, (int)pointRight.Value.Y);
-                                        }
-                                    }
-                                    catch { }
-                                }
-                            });
-
-                        }
-                    }
-                    */
 
                 }
                 //MessageBox.Show("device open end");
@@ -214,4 +151,53 @@ namespace K4ACalibration
         {
         }
     }
+
+    /*
+                    var calibration = device.GetCalibration();
+                    Transformation transformation = calibration.CreateTransformation();
+
+                    Tracker tracker = Tracker.Create(calibration, new TrackerConfiguration()
+                    {
+                        ProcessingMode = TrackerProcessingMode.Gpu,
+                        SensorOrientation = SensorOrientation.Default
+                    });
+
+                    while (true)
+                    {
+                        Capture capture1 = device.GetCapture();
+
+                        tracker.EnqueueCapture(capture1);
+                        var frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false);
+                        if (frame != null)
+                        {
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                for (uint i = 0; i < frame.NumberOfBodies; i++)
+                                {
+                                    var bodyId = frame.GetBodyId(i);
+                                    var body = frame.GetBody(i);
+
+                                    var handLeft = body.Skeleton.GetJoint(JointId.HandLeft);
+                                    var handRight = body.Skeleton.GetJoint(JointId.HandRight);
+
+                                    var pointLeft = calibration.TransformTo2D(handLeft.Position, CalibrationDeviceType.Depth, CalibrationDeviceType.Color);
+                                    var pointRight = calibration.TransformTo2D(handRight.Position, CalibrationDeviceType.Depth, CalibrationDeviceType.Color);
+
+                                    try
+                                    {
+                                        if (pointLeft.HasValue && pointRight.HasValue)
+                                        {
+                                            Image1.Source = CreateNewBitmap(capture1.Color,
+                                                (int)pointLeft.Value.X, (int)pointLeft.Value.Y,
+                                                (int)pointRight.Value.X, (int)pointRight.Value.Y);
+                                        }
+                                    }
+                                    catch { }
+                                }
+                            });
+
+                        }
+                    }
+                    */
+
 }
